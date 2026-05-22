de6771e4a1239051a0e19c9255ae67a4e35b7eee
Arush Anand <arushanand107@gmail.com>
2026-05-22 15:26:16 +0530

    Solved Move Zeroes

---
+++ Week 1/MoveZeroes.cpp
@@ -0,0 +1,16 @@
+// Solved using Parallel Pointers
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+        for(int right = 0; right < nums.size(); right++){
+            if(nums[right]!= 0){
+                int tmp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = tmp;
+                left++;
+            }
+        }
+    }
+};
