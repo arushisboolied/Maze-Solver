2fa179461ec11af2aefe1e3db5ec7aa76c72d018
Arush Anand <arushanand107@gmail.com>
2026-05-22 18:55:32 +0530

    Solved Three Sum

---
+++ Week 1/ThreeSum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> output;
+        int n = nums.size();
+
+        for (int i = 0; i + 2 < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    output.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                } 
+                else if (sum < 0)
+                    left++;
+                else 
+                    right--;
+            }
+        }
+        return output;
+    }
+};
