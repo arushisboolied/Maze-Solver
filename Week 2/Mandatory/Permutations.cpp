cb4dd8cd6d6f726c7b2c4144756aecdd475cd229
Arush Anand <arushanand107@gmail.com>
2026-06-03 22:43:28 +0530

    Solved Permutations

--- Week 2/Mandatory/Permutations.cpp
+++ Week 2/Mandatory/Permutations.cpp
@@ -3,6 +3,7 @@ public:
     void getPerms(vector<int>& nums, int idx, vector<vector<int>>& ans) {
         if (idx == nums.size()) {
             ans.push_back({nums});
+            return;
         }
         for (int i = idx; i < nums.size(); i++) {
             swap(nums[idx], nums[i]);
