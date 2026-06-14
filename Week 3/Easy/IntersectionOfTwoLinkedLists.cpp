a4826be9662a6179c7ee5ea572fb563cc0eb5c29
Arush Anand <arushanand107@gmail.com>
2026-06-15 01:18:33 +0530

    Solved Intersection of Two Linked Lists

---
+++ Week 3/Easy/IntersectionOfTwoLinkedLists.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* currA = headA;
+        ListNode* currB = headB;
+
+        while(currA != currB){
+            if(currA)
+                currA = currA->next;
+            else
+                currA = headB;
+            if(currB)
+                currB = currB->next;
+            else
+                currB = headA;
+        }
+        return currA;
+    }
+};
