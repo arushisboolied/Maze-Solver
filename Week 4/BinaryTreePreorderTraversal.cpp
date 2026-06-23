91e65562b80a07650632a542e25e78298e5a2b5c
Arush Anand <arushanand107@gmail.com>
2026-06-23 21:11:38 +0530

    Solved Binary Tree Preorder Traversal

---
+++ Week 4/BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if (!root)
+            return {};
+        vector<int> ans;
+        stack<TreeNode*> st;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* n = st.top();
+            st.pop();
+            ans.push_back(n->val);
+            if (n->right)
+                st.push(n->right);
+            if (n->left)
+                st.push(n->left);
+        }
+        return ans;
+    }
+};
