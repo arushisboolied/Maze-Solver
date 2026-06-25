fef33ebedff48aa1c9eb5a880064f3598e18fc68
Arush Anand <arushanand107@gmail.com>
2026-06-25 21:27:07 +0530

    Solved Lowest Common Ancestor of a Binary Tree

---
+++ Week 4/Mandatory/LowestCommonAncestoryOfABinaryTree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q)
+            return root;
+
+        TreeNode* leftLCA = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightLCA = lowestCommonAncestor(root->right, p, q);
+
+        if(!leftLCA)
+            return rightLCA;
+        if(!rightLCA)
+            return leftLCA;
+        return root;
+    }
+};
