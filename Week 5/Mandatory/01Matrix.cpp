175c6bb36962b85148f780de8dcde1e110af8759
Arush Anand <arushanand107@gmail.com>
2026-07-15 18:32:42 +0530

    Solved 01 Matrix

---
+++ Week 5/Mandatory/01Matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> DIR = {0, 1, 0, -1, 0};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& adj) {
+        int m = adj.size(), n = adj[0].size();
+        queue<pair<int, int>> q;
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                if (adj[r][c] == 0)
+                    q.push({r, c});
+                else
+                    adj[r][c] = -1;
+
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+            for (int i = 0; i < 4; ++i) {
+                int nr = r + DIR[i], nc = c + DIR[i + 1];
+                if (nr < 0 || nr == m || nc < 0 || nc == n || adj[nr][nc] != -1)
+                    continue;
+                adj[nr][nc] = adj[r][c] + 1;
+                q.push({nr, nc});
+            }
+        }
+        return adj;
+    }
+};
